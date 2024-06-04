6fdfdbbceb4a5c75f58229cec72d27e50bf3c71a
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-06-04 22:14:34 +0900

    UserControllerの作成とapi.phpの編集

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\UserController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
