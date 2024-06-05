2d64c1727b0f398f9d9d3682ffd89edcb16605da
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-06-05 20:46:02 +0900

    ログイン、ログアウト昨日の作成

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -3,19 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
 {
-
-    public function login(Reqeust $request)
+    public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+        $user = Auth::user();
+
+        if ($user->is_admin) {
+            // 管理者用のレスポンス
+            $token = $user->createToken('API Token',['admin'])->plainTextToken;
+            return response()->json(['token' => $token, 'message' => 'Admin logged in'], 200);
+        } else {
+            // 一般ユーザー用のレスポンス
+            $token = $user->createToken('API Token',['user'])->plainTextToken;
+            return response()->json(['token' => $token, 'message' => 'User logged in'], 200);
+        }
+    } else {
+        return response()->json(['message' => 'Invalid credentials'], 401);
     }
-    public function logout(Reqeust $request)
+}
+
+    public function logout(Request $request)
     {
-        //
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,12 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\UserLoginController;
+use App\Http\Controllers\UserLoginController;
 use App\Http\Controllers\UserRegisterController;
 
 Route::post('/register', [UserRegisterController::class, 'register']);
+Route::post('/login', [UserLoginController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [UserLoginController::class, 'logout']);
 
 Route::get('/admin/posts', [AdminController::class, 'indexPost']);
 Route::post('/admin/posts', [AdminController::class, 'storePost']);
