39de2ee8290d1f312cda30b03b7a0e35284b3cfd
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-06-06 19:52:40 +0900

    モデルとマイグレーションの追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,14 @@ class Post extends Model
 {
     use HasFactory;
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
     protected $fillable = ['topic_id', 'user_id', 'post_content', ];
 
     protected $dates = ['created_at', 'updated_at'];
