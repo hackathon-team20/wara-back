525ec889454466bbaf15ed86a863222e56f1af56
HashimotoShuhei-z <hassii0510@outlook.jp>
2024-06-09 15:18:23 +0900

    bag

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,26 +20,22 @@ class UserController extends Controller
     {
         //ログイン中のユーザーを取得
         $userId = auth()->id();
+        //投稿一覧を取得
+        $query = Post::query();
+        $query = $query->with(['user', 'reviews'])
+            ->orderBy('created_at', 'desc')
+                ->get();
 
-        // 最新のトピックを取得
-        $topic = Topic::orderBy('created_at', 'desc')->first();
-
-        // 最新のトピックに紐づく投稿一覧を取得
-        $posts = Post::where('topic_id', $topic->id)
-        ->with(['user', 'reviews'])
-        ->orderBy('created_at', 'desc')
-        ->get();
-
-        // 各投稿に対してログインユーザーがレビューをつけているかを確認
-        $posts->each(function ($post) use ($userId) {
-            $post->isReviewedByUser = $post->reviews->where('user_id', $userId)->isNotEmpty();
-            // reviewsコレクションをメモリから解放する
-            unset($post->reviews);
-        });
+        //各投稿に対してログインユーザーがレビューをつけているかを確認
+        $query->each(function ($query) use ($userId) {
+        $query->isReviewedByUser = $query->reviews->where('user_id', $userId)->isNotEmpty();
+        //reviewsコレクションをメモリから解放する
+        unset($query->reviews);
+    });
 
         return response()->json(
             [
-                'posts' => $posts,
+                'post' => $query,
             ],
             200
         );
